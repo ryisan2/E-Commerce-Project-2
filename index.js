4348a43494eaf5ddd8f801b819d86632af6785b1
rysian2 <ryisan2@gmail.com>
2024-05-12 11:45:31 -0400

    New stuff

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-/* We are using javasript to open and close a menu */
-
 function openMenu() {
-    document.body.classList += " menu--open"
-  }
-  
-  function closeMenu() {
-    document.body.classList.remove('menu--open')
-  }
+  document.body.classList += " menu--open"
+}
+
+function closeMenu() {
+  document.body.classList.remove('menu--open')
+}
